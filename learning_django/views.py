7f75b41133f11406401bfff12bd4a52965d38d4a
JeetDodiy <jeetdodiya660741@gmail.com>
2026-06-15 17:45:08 +0530

    yaa boy

--- learning_django/views.py
+++ learning_django/views.py
@@ -1,1 +1,25 @@
-from django import Httperesponse
+from django.http import HttpResponse
+from django.shortcuts import render
+from numpy import char
+
+
+def index(request):
+    return render(request, "first.html")
+
+def home(request):
+    djtext = request.GET.get('text','fu')
+    djremovepunc = request.GET.get('removepunc','0ff')
+    print(djtext)
+    print(djremovepunc)
+    punctuation = '''!()-[]{};:'",<>. /?@#$%^&*_~'''
+    analyzed = ""
+    for char in djtext:
+        if char not in punctuation:
+            analyzed = analyzed + char
+    param = {'purpose':'this is that', 'analyze_text': analyzed}
+    return render(request, "analyze.html",param)
+
+# def analyze(request):
+#     return render(request, "analyze.html")
+
+

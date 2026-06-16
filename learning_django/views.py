8982b963b3c564f62f547d915eb106c602e95cfd
JeetDodiy <jeetdodiya660741@gmail.com>
2026-06-16 18:16:01 +0530

    Add sumthing

--- learning_django/views.py
+++ learning_django/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from numpy import char
-
 
 def index(request):
     return render(request, "first.html")
@@ -9,15 +7,16 @@ def index(request):
 def home(request):
     djtext = request.GET.get('text','fu')
     djremovepunc = request.GET.get('removepunc','0ff')
-    print(djtext)
-    print(djremovepunc)
-    punctuation = '''!()-[]{};:'",<>. /?@#$%^&*_~'''
-    analyzed = ""
-    for char in djtext:
-        if char not in punctuation:
-            analyzed = analyzed + char
-    param = {'purpose':'this is that', 'analyze_text': analyzed}
-    return render(request, "analyze.html",param)
+    if djremovepunc == 'on':
+        punctuation = '''!()-[]{};:'",<>.?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuation:
+                analyzed = analyzed + char
+        param = {'purpose':'this is that', 'analyze_text': analyzed}
+        return render(request, "analyze.html",param)
+    else:
+        return HttpResponse("Error")
 
 # def analyze(request):
 #     return render(request, "analyze.html")

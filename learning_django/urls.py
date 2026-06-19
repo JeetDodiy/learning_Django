c6b9c3b3602cb11e2e30dcc8be7bfac0f8a01f31
JeetDodiy <jeetdodiya660741@gmail.com>
2026-06-19 14:41:05 +0530

    learn to add bootstrap

--- learning_django/urls.py
+++ learning_django/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
+    path('index',index.index,name='index'),
 ]
